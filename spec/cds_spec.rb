be7f276e14c79e53e360b4636049308492f3ab9e
Sasha Burchuk & David Zulaica <student@epicodus.com>
2015-01-15 11:28:59 -0800

    Revise spec description for clarity.

--- spec/cds_spec.rb
+++ spec/cds_spec.rb
@@ -3,7 +3,7 @@ require("cds")
 
 describe("CD") do
   describe("#store") do
-    it("Stores all of the attributes of a CD") do
+    it("creates a CD class and saves it into the collection array.") do
       test_cd = CD.new({:artist => "Artist", :album => "Album", :year => 2001 })
       test_cd.store()
       expect(CD.all()).to(eq([test_cd]))
