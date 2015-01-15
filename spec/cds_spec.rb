59f801854126da64e024e07c34256b8989e8cb0b
Sasha Burchuk & David Zulaica <student@epicodus.com>
2015-01-15 12:30:28 -0800

    Add test for fetch method.

--- spec/cds_spec.rb
+++ spec/cds_spec.rb
@@ -14,4 +14,19 @@ describe("CD") do
     end
   end
 
+  describe("#fetch") do
+    it("returns a CD by the artist specified") do
+      test_cd = CD.new({:artist => "Artist", :album => "Album", :year => 2001 })
+      test_cd.store()
+      other_cd = CD.new({:artist => "Craving the Seamstress", :album => "EP 5-15-04", :year => 2004})
+      other_cd.store()
+      expect(CD.fetch("artist", "Craving the Seamstress")).to(eq([other_cd.artist(), other_cd.album(), other_cd.year()]))
+    end
+
+    it("returns a CD by the album specified") do
+      test_cd = CD.new({:artist => "Teenage Fanclub", :album => "Songs From Northern Britain", :year => 1996})
+      test_cd.store()
+      expect(CD.fetch("album", "Songs From Northern Britain")).to(eq([test_cd.artist(), test_cd.album(), test_cd.year()]))
+    end
+  end
 end
