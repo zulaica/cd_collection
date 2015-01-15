7606dcbf895228d014181e42496041a158e320f7
Sasha Burchuk & David Zulaica <student@epicodus.com>
2015-01-15 11:35:51 -0800

    Add clear method on before method to clear out object

--- spec/cds_spec.rb
+++ spec/cds_spec.rb
@@ -2,6 +2,10 @@ require("rspec")
 require("cds")
 
 describe("CD") do
+  before() do
+    CD.clear()
+  end
+
   describe("#store") do
     it("creates a CD class and saves it into the collection array.") do
       test_cd = CD.new({:artist => "Artist", :album => "Album", :year => 2001 })
