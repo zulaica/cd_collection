c95b5393a85ceb0712a45e5199891c163a2b7c90
Sasha Burchuk & David Zulaica <student@epicodus.com>
2015-01-15 17:24:13 -0800

    Revise specs to work with refactored fetch method.

--- spec/cds_spec.rb
+++ spec/cds_spec.rb
@@ -20,13 +20,13 @@ describe("CD") do
       test_cd.store()
       other_cd = CD.new({:artist => "Craving the Seamstress", :album => "EP 5-15-04", :year => 2004})
       other_cd.store()
-      expect(CD.fetch("artist", "Craving the Seamstress")).to(eq([[other_cd.artist(), other_cd.album(), other_cd.year()]]))
+      expect(CD.fetch("artist", "Craving the Seamstress")).to(eq([other_cd]))
     end
 
     it("returns a CD by the album specified") do
       test_cd = CD.new({:artist => "Teenage Fanclub", :album => "Songs From Northern Britain", :year => 1996})
       test_cd.store()
-      expect(CD.fetch("album", "Songs From Northern Britain")).to(eq([[test_cd.artist(), test_cd.album(), test_cd.year()]]))
+      expect(CD.fetch("album", "Songs From Northern Britain")).to(eq([test_cd]))
     end
 
     it("returns multiple CDs by the artist specified") do
@@ -34,7 +34,7 @@ describe("CD") do
       test_cd.store()
       other_cd = CD.new({:artist => "Man… or Astro-Man?", :album => "Project Infinity", :year => 1995})
       other_cd.store()
-      expect(CD.fetch("artist", "Man… or Astro-Man?")).to(eq([[test_cd.artist(), test_cd.album(), test_cd.year()], [other_cd.artist(), other_cd.album(), other_cd.year()]]))
+      expect(CD.fetch("artist", "Man… or Astro-Man?")).to(eq([test_cd, other_cd]))
     end
   end
 end
